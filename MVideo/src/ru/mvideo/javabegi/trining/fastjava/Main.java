ef8071c4e96a02939e6f1e3d40188e87ab021b67
ShestopalovAnton <shestopalovayu@gmail.com>
2025-07-19 16:37:23 +0300

    File TASK

--- MVideo/src/ru/mvideo/javabegi/trining/fastjava/Main.java
+++ MVideo/src/ru/mvideo/javabegi/trining/fastjava/Main.java
@@ -1,5 +1,6 @@
 package ru.mvideo.javabegi.trining.fastjava;
 
+import ru.mvideo.javabegi.trining.fastjava.objects.Apple;
 import ru.mvideo.javabegi.trining.fastjava.objects.Cashier;
 
 public class Main {
@@ -11,11 +12,30 @@ public class Main {
 		
 		cashier_1.setName("Мария Петровна");
 		cashier_1.setExperience("3 года");
+		cashier_1.setMoney(10000);
 		
 		System.out.println("Кассир " + cashier_1.getName() + " стаж - " + cashier_1.getExperience());
 		System.out.println("Денег у кассира: " + cashier_1.getMoney());
 		
 		//cashier_1.sellProduct(5000);
 		System.out.println("Номер чека: " + cashier_1.sellProduct(5000));
+		
+		Apple phone1 = new Apple();
+		
+		phone1.setModel("iPhone 16 Pro Max");
+		phone1.setColor("Black");
+		phone1.setProcessor("A18 Pro");
+		phone1.setRom(256);
+		phone1.setCamera(48);
+		phone1.setQuantity(10);
+		
+		System.out.println("\nХарактеристики: ");
+		System.out.println("Модель - " + phone1.getModel());
+		System.out.println("Цвет - " + phone1.getColor());
+		System.out.println("Процессор - " + phone1.getProcessor());
+		System.out.println("Память - " + phone1.getRom());
+		System.out.println("Касмера - " + phone1.getCamera() + "\n");
+		
+		phone1.takePhone(2);
 	}
 }
